d012d75ae90715a3066753af5c7499e25928bde7
Trevor Gamblin <tvgamblin@gmail.com>
2022-12-02 08:31:16 -0500

    Remove vim_setup.sh; add nerdtree, gitgutter, fugitive to new one
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- vim-setup.sh
+++ vim-setup.sh
@@ -7,9 +7,22 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # import .vimrc
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/threexc/devtoolconfig/master/.vimrc
 
-# clone vim-airline unless it already exists for some reason
-DIR="$HOME/.vim/bundle/vim-airline"
-if [ ! -d "$DIR" ]; then
-    echo "$DIR does not exist."
+# clone vim-airline unless it already exists
+if [ ! -d "$HOME/.vim/bundle/vim-airline" ]; then
     git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+fi
+
+# clone fugitive unless it already exists
+if [ ! -d "$HOME/.vim/bundle/vim-fugitive" ]; then
+    git clone https://tpope.io/vim/fugitive.git ~/.vim/bundle/vim-fugitive
+fi
+
+# clone nerdtree unless it already exists
+if [ ! -d "$HOME/.vim/bundle/nerdtree" ]; then
+    git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+fi
+
+# clone vim-gitgutter unless it already exists
+if [ ! -d "$HOME/.vim/bundle/vim-gitgutter" ]; then
+    git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 fi
