dfe59401a2d8d471c852dc2de0190ffea244fc18
Trevor Gamblin <tvgamblin@gmail.com>
2023-06-21 14:00:07 -0400

    Add more libraries and reorg
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- system_setup.sh
+++ system_setup.sh
@@ -3,17 +3,21 @@ set -x
 
 sudo dnf -y update
 sudo dnf -y groupinstall "Development Tools"
-sudo dnf install -y acpica-tools b4 bison boost boost-devel bzip2 capnproto \
-    capnproto-devel ccache chrpath cinnamon cmake cpio cpp dfu-util diffutils \
-    diffstat dwarves elfutils-devel file findutils flex git git-email gawk \
-    gcc gcc-c++ gdb glibc-devel glibc-langpack-en golang gzip hostname htop \
-    iproute-tc kernel-headers lz4 libuuid-devel make mesa-libGL-devel \
-    nasm ncurses-devel ninja-build openssl-devel patch perl perl-bignum \
-    perl-Data-Dump perl-FindBin perl-File-Compare perl-File-Copy \
-    perl-locale perl-Text-ParseWords perl-Thread-Queue python3 \
-    python3-GitPython python3-jinja2 python3-pexpect python3-pip \
-    rapidjson-devel rpcgen SDL-devel sqlite-devel socat tar texinfo \
-    thunderbird tmux unzip vim-enhanced wget which xz zstd
+sudo dnf install -y acpica-tools b4 bison boost boost-devel bzip2 \
+    capnproto capnproto-devel ccache chrpath cinnamon cmake cpio cpp \
+    dfu-util diffutils diffstat dwarves elfutils-devel file findutils \
+    flex git git-email gawk gcc gcc-c++ gdb glib-devel glib2-devel \
+    glibc-devel glibc-langpack-en golang gnutls-devel \
+    gsettings-desktop-schemas-devel gzip hostname htop iproute-tc \
+    kernel-headers libguestfs libuuid-devel libproxy-devel lz4 make \
+    mesa-libGL-devel meson nasm ncurses-devel ninja-build \
+    openssl-devel patch perl perl-bignum perl-Data-Dump \
+    perl-FindBin perl-File-Compare perl-File-Copy perl-locale \
+    perl-Text-ParseWords perl-Thread-Queue pixman-devel \
+    python3 python3-GitPython python3-jinja2 python3-pexpect \
+    python3-pip rapidjson-devel rpcgen SDL-devel sqlite-devel \
+    socat tar texinfo thunderbird tmux unzip vim-enhanced wget \
+    which xz zstd
 
 set +x
 read -p "Set hostname? " yn
