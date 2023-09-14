713de3ba7f1b1cdccb2336798ac740629f6bca0a
Trevor Gamblin <tvgamblin@gmail.com>
2023-09-14 14:35:19 -0400

    vim-setup.sh: Don't pull .vimrc from remote
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- vim-setup.sh
+++ vim-setup.sh
@@ -4,9 +4,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# import .vimrc
-curl -LSso ~/.vimrc https://raw.githubusercontent.com/threexc/devtoolconfig/master/.vimrc
-
 # clone vim-airline unless it already exists
 if [ ! -d "$HOME/.vim/bundle/vim-airline" ]; then
     git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
@@ -26,3 +23,5 @@ fi
 if [ ! -d "$HOME/.vim/bundle/vim-gitgutter" ]; then
     git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 fi
+
+cp .vimrc ~/.vimrc
